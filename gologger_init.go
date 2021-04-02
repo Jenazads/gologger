98e25796e0a0af80bb734aa02a34dfa48cc55fed
Felipe A. Moreno <fmorenovr@gmail.com>
2021-04-02 17:16:16 -0300

    Create gologger_init.go

---
+++ gologger_init.go
@@ -0,0 +1,10 @@
+package gologger
+
+import(
+//  "fmt";
+)
+
+func init() {
+  //fmt.Println("====> Init GoLogger, a simple ayer logger for you development :D")
+  //fmt.Println("====> by Jenazads.\n")
+}
